05afafadb40aa7f71b8e634915e3d4e8cccdf93d
Prabir Ghosh <45656188+pgsgcg@users.noreply.github.com>
2024-08-16 09:41:53 +0530

    Add files via upload

---
+++ login-backend-app/src/main/java/in/mahesh/tasks/ReactLoginApiApplication.java
@@ -0,0 +1,15 @@
+package in.mahesh.tasks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class ReactLoginApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactLoginApiApplication.class, args);
+	}
+
+}
